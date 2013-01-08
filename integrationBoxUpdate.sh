948227e143a52b1c7f4502eb8fda0fe974055de8
myles.bostwick <myles.bostwick@schneider-electric.com>
2013-01-08 11:12:34 -0700

    Add some updates to gitcheck.

--- integrationBoxUpdate.sh
+++ integrationBoxUpdate.sh
@@ -4,7 +4,9 @@ WS='/home/myles/workspace/webapp'
 BUILDDIR="$WS/build"
 MIRRORBASE='http://mirror.pelco.org/yapt/endura-pl4-x86_64'
 RPMFOLDER=''
-choice='t'
+remote_choice='b'
+local_choice='b'
+
 # Check for a host
 if [[ $# -lt 1 ]]; then
     echo "Must specify host."
@@ -14,18 +16,26 @@ fi
 if [[ $# -ge 3 ]]; then
     read res'?Trunk or branch?(t/b): '
 else
-    echo "Using $choice"
-    res=$choice
+    echo "Using remote $remote_choice local $local_choice"
+    res=$remote_choice
 fi
 
 # Looking for trunk dir
-if [[ $res == 't' ]]; then
-    WEBDIR=`find $BUILDDIR -maxdepth 1 -type d -name '*SNAPSHOT*'`
+if [[ $remote_choice == 't' ]]; then
     RPMFOLDER="$MIRRORBASE/webapp-SNAPSHOT"
 
-elif [[ $res == 'b' ]]; then
-    WEBDIR=`find $BUILDDIR -maxdepth 1 -type d -name 'webapp-[0-9]\.*'`
+elif [[ $remote_choice == 'b' ]]; then
     RPMFOLDER="$MIRRORBASE/webapp"
+else
+    echo "Invalid selection."
+    return 1
+fi
+
+if [[ $local_choice == 't' ]]; then
+    WEBDIR=`find $BUILDDIR -maxdepth 1 -type d -name '*SNAPSHOT*'`
+
+elif [[ $local_choice == 'b' ]]; then
+    WEBDIR=`find $BUILDDIR -maxdepth 1 -type d -name 'webapp-[0-9]\.*'`
 else
     echo "Invalid selection."
     return 1
@@ -40,6 +50,10 @@ if [[ $# -ge 2 ]]; then
         rest='/`productconfig | grep webapp`.noarch.rpm && rpm -e `rpm -qa | grep webapp`'
         /usr/bin/ssh -l root $1 "$wget$RPMFOLDER$rest"
     elif [[ $2 == 'restore' ]]; then
+
+        echo "Removing var/html from $1"
+        /usr/bin/ssh -l root $1 'rm -rf /var/www/html/*'
+
         echo "Restoring from backup."
         # re-install from backed-up RPM
         /usr/bin/ssh -l root $1 'rm -rf /var/www/html/* && rpm -ivh `ls | grep webapp` && rm `ls | grep webapp`'
@@ -58,4 +72,4 @@ fi
 echo "Removing var/html from $1"
 /usr/bin/ssh -l root $1 'rm -rf /var/www/html/*'
 echo "Copying to $1"
-/usr/bin/scp -r $WEBDIR/sm5200 $WEBDIR/ExpandedView $WEBDIR/*.html $WEBDIR/*.css $WEBDIR/public root@$1:/var/www/html/
+/usr/bin/scp -r $WEBDIR/sm5200 $WEBDIR/*.html $WEBDIR/*.css $WEBDIR/public root@$1:/var/www/html/
